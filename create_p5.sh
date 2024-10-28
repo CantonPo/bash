76f1389f8c20215cea8d50eb157833bfef404c88
CantonPo <cantoncorpas@gmail.com>
2024-10-28 09:37:20 +0100

    repo de bash creando p5

---
+++ create_p5.sh
@@ -0,0 +1,18 @@
+echo "function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}"
+
+
+
+
+echo "function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}"> p5/sketch.js
---
+++ ginit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+nombre_proyecto=$1
+
+echo "Valor de entrada: $1"
+
+mkdir $1
+cd $1
+git init
+touch index.html
+touch script.js
+git add .
+git commit -m "build"
+#para ejecutarlo nos pide permisos
